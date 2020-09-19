c754716a4857394a0dac50996bf4f403685befe3
covector <60296643+covector@users.noreply.github.com>
2020-09-19 20:16:51 +0800

    added enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public Transform obj;
+    public float discount;
+    public float speed;
+    public void face()
+    {
+        Vector2 vec = target.position - obj.position;
+        float targetAngle = Mathf.Atan2(vec.y, vec.x) * 180 / Mathf.PI;
+        targetAngle = (targetAngle + 360) % 360;
+        float viewAngle = obj.eulerAngles.z;
+        int quad1t = targetAngle < 90f ? 1 : 0;
+        int quad1v = viewAngle < 90f ? 1 : 0;
+        float threshold = targetAngle * quad1t + viewAngle * quad1v + 180f;
+        int quad4 = (targetAngle > threshold | viewAngle > threshold) ? 1 : 0;
+        targetAngle -= quad1v * quad4 * 360f;
+        viewAngle -= quad1t * quad4 * 360f;
+        float delta = targetAngle - viewAngle;
+        viewAngle += delta * discount; //Exponential / Frame Dependent
+
+        //float deltaScaled = Mathf.Sign(delta) * discount * Time.deltaTime;
+        //viewAngle += Mathf.Min(delta, deltaScaled); //Linear / Frame Independent
+        obj.eulerAngles =  new Vector3(0f, 0f, viewAngle);
+    }
+    public void walk()
+    {
+        float radAng = obj.eulerAngles.z * Mathf.PI / 180;
+        obj.position += speed * Time.deltaTime * new Vector3(Mathf.Cos(radAng), Mathf.Sin(radAng), 0);
+    }
+
+    void Update()
+    {
+        face();
+        walk();
+    }
+}
