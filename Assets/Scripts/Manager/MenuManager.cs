02d31b84c0c453c77b9248499a759e5a0216f9da
covector <60296643+covector@users.noreply.github.com>
2020-09-24 17:26:20 +0800

    main menu

---
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Animator transition;
+    private float transitionPeriod = 0.5f;
+    public void StartGame()
+    {
+        StartCoroutine(GoScene(1));
+    }
+    public void QuitGame()
+    {
+        StartCoroutine(QuitGameCoroutine());
+    }
+    public void GoSkin()
+    {
+        StartCoroutine(GoScene(2));
+    }
+    public void GoOption()
+    {
+        StartCoroutine(GoScene(3));
+    }
+    public IEnumerator GoScene(int sceneIndex)
+    {
+        transition.SetTrigger("StartTrans");
+        yield return new WaitForSeconds(transitionPeriod);
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public IEnumerator QuitGameCoroutine()
+    {
+        transition.SetTrigger("StartTrans");
+        yield return new WaitForSeconds(transitionPeriod);
+        Application.Quit();
+    }
+}
