0a344990db59f0de3bfb481c029dc4d51977f8ea
covector <60296643+covector@users.noreply.github.com>
2021-02-22 21:01:39 +0800

    reorganize folders

---
+++ Assets/Entity/Camera/Scripts/CameraShake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private float timePassed;
+    private IEnumerator Shake(float time, float magnitude)
+    {
+        float variation = Random.Range(0f, 100f);
+        timePassed = 0;
+        while (timePassed < time)
+        {
+            float decay = magnitude * (1 - timePassed / time);
+            transform.localPosition = new Vector3(decay * (Mathf.PerlinNoise(variation + 20f * timePassed, 0) - 0.5f), decay * (Mathf.PerlinNoise(0, variation + 20f * timePassed) - 0.5f),  -10);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+    }
+    public void InitShake(float time, float magnitude)
+    {
+        StartCoroutine(Shake(time, magnitude));
+    }
+}
